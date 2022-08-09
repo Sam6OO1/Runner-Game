c6e2f23ae420a0491462e992421f42ec06406031
Samuel <samuelnuade@rhs.school.nz>
2022-08-09 14:17:35 +1200

    Model and Particle Update
    Teleporter Model, Particle and sound effect
    
    Explosive Barrel Model and Texture

--- Assets/MyAssets/Scripts/Teleporter.cs
+++ Assets/MyAssets/Scripts/Teleporter.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-   
+    public AudioSource teleporterSEffect;
+    public ParticleSystem teleporterEffect;
+    public float teleporterDelay = 2f;
+
     [SerializeField] private Transform player;
 
     [SerializeField] private Transform teleportPoint;
 
-
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(teleporterDelay);
+        
+        player.transform.position = teleportPoint.transform.position;
+        teleporterSEffect.Play();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+          
+            teleporterEffect.Play();
+            StartCoroutine(Wait());
+            return;
+           
+
             
-            player.transform.position = teleportPoint.transform.position;
         }
     }
 }
